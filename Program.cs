8440cb56debc17803eb837e458b6d80a23a5feee
s0rs00d <ottohuusko@gmail.com>
2022-10-05 15:54:49 +0300

    faker harjoitus

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        for (int i = 0; i < 3; i++) 
+        {
+            Console.WriteLine($"Terve, nimeni on {Faker.Name.FullName()}, asun {Faker.Address.City()}, ja sosiaaliturvatunnukseni on {Faker.Identification.SocialSecurityNumber()}");
+        }
+    }
+}
