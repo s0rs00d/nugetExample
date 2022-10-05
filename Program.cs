8e9eabc98f6228d7a5976d4238a5c61ecbd0a44d
s0rs00d <ottohuusko@gmail.com>
2022-10-05 16:10:10 +0300

    add some examples

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿internal class Program
 {
+    //Näin voi hakea ja laittaa muuttujille "faker arvot"
+    //var nimi = Faker.Name.FullName();
+    //var osoite = Faker.Address.City();
+    //var sosnumero = Faker.Identification.SocialSecurityNumber();
+
+
     private static void Main(string[] args)
-    {   
+    {
         for (int i = 0; i < 3; i++) 
         {
             Console.WriteLine($"Terve, nimeni on {Faker.Name.FullName()}, asun {Faker.Address.City()}, ja sosiaaliturvatunnukseni on {Faker.Identification.SocialSecurityNumber()}");
